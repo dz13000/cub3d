23dc58eca6ba28242737cbb9ae4b762bb65379bd
dz13000 <kamilbop93@gmail.com>
2023-12-09 19:05:58 +0100

    en plein exec

--- cub3d.h
+++ cub3d.h
@@ -23,6 +23,29 @@
 #  define BUFFER_SIZE 1
 // # endif
 
+#define ROTATE_LEFT 65361
+#define ROTATE_RIGHT 65363
+#define FORWARD_W_Z 119
+#define BACK_S_S 115
+#define RIGHT_D_D 100
+#define LEFT_A_Q 97
+# define WIDTH 1680
+# define HEIGHT 680
+
+typedef struct		s_img
+{
+	void			*mlx_ptr;
+	void			*mlx_win;
+	void			*img;
+	int				*addr;
+	int				bits_per_pixel;
+	int				line_length;
+	int		size_l;
+	int				endian;
+	int		img_width;
+	int		img_height;
+}					t_img;
+
 typedef struct s_cub
 {
 	char	*map;
@@ -46,9 +69,47 @@ typedef struct s_cub
 
 }			t_cub;
 
+typedef struct	s_ray
+{
+	int		**buf;
+	int		**texture;
+	double	movespeed;
+	double	rotspeed;
+	char	*paths[4];
+	double		posx; //position x du joueur
+	double		posy; //position y du joueur
+	double		dirx; //vecteur de direction (commence à -1 pour N, 1 pour S, 0 sinon)
+	double		diry; //vecteur de direction (commence à -1 pour W, 1 pour E, 0 sinon)
+	double		planx; //vecteur du plan (commence à 0.66 pour E, -0.66 pour W, 0 sinon)
+	double		plany; //vecteur du plan (commence à 0.66 pour N, -0.66 pour S, 0 sinon)
+	double		raydirx; //calcul de direction x du rayon
+	double		raydiry; //calcul de direction y du rayon
+	double		camerax; //point x sur la plan camera : Gauche ecran = -1, milieu = 0, droite = 1
+	int		mapx; // coordonée x du carré dans lequel est pos
+	int		mapy; // coordonnée y du carré dans lequel est pos
+	double		sidedistx; //distance que le rayon parcours jusqu'au premier point d'intersection vertical (=un coté x)
+	double		sidedisty; //distance que le rayon parcours jusqu'au premier point d'intersection horizontal (= un coté y)
+	double		deltadistx; //distance que rayon parcours entre chaque point d'intersection vertical
+	double		deltadisty; //distance que le rayon parcours entre chaque point d'intersection horizontal
+	int		stepx; // -1 si doit sauter un carre dans direction x negative, 1 dans la direction x positive
+	int		stepy; // -1 si doit sauter un carre dans la direction y negative, 1 dans la direction y positive
+	int		hit; // 1 si un mur a ete touche, 0 sinon
+	int		side; // 0 si c'est un cote x qui est touche (vertical), 1 si un cote y (horizontal)
+	double		perpwalldist; // distance du joueur au mur
+	int		lineheight; //hauteur de la ligne a dessiner
+	int		drawstart; //position de debut ou il faut dessiner
+	int		drawend; //position de fin ou il faut dessiner
+	int		x; //permet de parcourir tous les rayons
+}					t_ray;
+
 typedef struct t_exec
 {
+	void	*mlx;
+	void	*window;
 	char	**final_map;
+	char 	letter;
+	t_img	img;
+	t_ray	ray;
 }			t_exec;
 
 char		*get_next_line(int fd);
--- graph/init.c
+++ graph/init.c
@@ -12,7 +12,160 @@
 
 #include "../cub3d.h"
 
+void    check_letter(t_exec *exec)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while(exec->final_map[i])
+    {
+        j = 0;
+        while(exec->final_map[i][j])
+        {
+            if(exec->final_map[i][j] == 'N')
+                exec->letter = 'N';
+            if(exec->final_map[i][j] == 'S')
+                exec->letter = 'S';
+            if(exec->final_map[i][j] == 'W')
+                exec->letter = 'W';
+            if(exec->final_map[i][j] == 'E')
+                exec->letter = 'E';
+            j++;
+        }
+        i++;
+    }
+}
+
+void	view_letter(t_exec *exec)
+{
+	if (exec->letter == 'N')
+	{
+		exec->ray.dirx = -1;
+		exec->ray.diry = 0;
+	}
+	else if (exec->letter == 'S')
+	{
+		exec->ray.dirx = 1;
+		exec->ray.diry = 0;
+	}
+	else if (exec->letter == 'W')
+	{
+		exec->ray.dirx = 0;
+		exec->ray.diry = -1;
+	}
+	else if (exec->letter == 'E')
+	{
+		exec->ray.dirx = 0;
+		exec->ray.diry = 1;
+	}
+}
+
+void    define_vector(t_exec *exec)
+{
+    if (exec->letter == 'N')
+	{
+		exec->ray.planx = 0;
+		exec->ray.plany = 0.66;
+	}
+	else if (exec->letter == 'S')
+	{
+		exec->ray.planx = 0;
+		exec->ray.plany = -0.66;
+	}
+	else if (exec->letter == 'W')
+	{
+		exec->ray.planx = -0.66;
+		exec->ray.plany = 0;
+	}
+	else if (exec->letter == 'E')
+	{
+		exec->ray.planx = 0.66;
+		exec->ray.plany = 0;
+    }
+}
+
+void	malloc_map(t_exec *exec)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	exec->ray.buf = malloc(sizeof(int **) * HEIGHT);
+	exec->ray.texture = malloc(sizeof(int **) * 4);
+	while (i < HEIGHT)
+    {
+		exec->ray.buf[i] = malloc(sizeof(int *) * WIDTH + 1);
+        i++;
+    }
+	i = 0;
+	while (i < 4)
+    {
+		exec->ray.texture[i] = malloc(sizeof(int *) * (64
+					* 64));
+        i++;
+    }
+	i = -1;
+	while (++i < 4)
+	{
+		j = -1;
+		while (++j < (64 * 64))
+			exec->ray.texture[i][j] = 0;
+	}
+}
+// modif
+void	load_image(t_exec *exec, int *texture, char *path, t_img *img)
+{
+	int	y;
+	int	x;
+
+	y = -1;
+	img->img = mlx_xpm_file_to_image(exec->mlx, path, &img->img_width,
+			&img->img_height);
+	if (!img->img)
+	{
+		printf("Error\nInvalid texture path\n");
+	}
+	img->addr = (int *)mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->size_l,
+			&img->endian);
+	while (++y < img->img_height)
+	{
+		x = -1;
+		while (++x < img->img_width)
+			texture[img->img_width * y + x] = img->addr[img->img_width * y + x];
+	}
+	mlx_destroy_image(exec->mlx, img->img);
+}
+
+void	load_texture(t_exec *exec)
+{
+	load_image(exec, exec->ray.texture[0], exec->ray.paths[0],
+		&exec->img);
+	load_image(exec, exec->ray.texture[1], exec->ray.paths[1],
+		&exec->img);
+	load_image(exec, exec->ray.texture[2], exec->ray.paths[2],
+		&exec->img);
+	load_image(exec, exec->ray.texture[3], exec->ray.paths[3],
+		&exec->img);
+}
+
 void    ft_init(t_exec *exec)
 {
-    (void)exec;
+    exec->mlx = mlx_init();
+    if(!exec->mlx)
+    {
+        //free exec;
+        exit(1);
+    }
+    check_letter(&*exec);
+    view_letter(&*exec);
+    define_vector(&*exec);
+    //
+    malloc_map(&*exec);
+    exec->ray.movespeed = 0.5;
+    exec->ray.rotspeed = 0.09;
+	exec->window = mlx_new_window(exec->mlx, WIDTH, HEIGHT, "cub3d");
+    //donner les  chemins des textures
+	load_texture(&*exec);
 }
